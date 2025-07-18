259e08ba501216ff28fdcfceffec4ba684e3b65d
Pablo Orviz <orviz@ifca.unican.es>
2025-07-18 14:03:36 +0200

    Add JePL file structure

--- .sqa/script.surly-beige-pinscher.sh
+++ .sqa/script.surly-beige-pinscher.sh
@@ -4,6 +4,6 @@
 # SPDX-License-Identifier: GPL-3.0-only
 
 (
-cd github.com/m-team-kit/drift-watch &&
-    find_doc_files.py --file_type all
+cd github.com/ai4os/ai4-docs &&
+    git rev-parse --is-inside-work-tree 
 )
